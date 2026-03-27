3316ec6a3fe85257c6e5a8809f8c83596bab4374
Learner12313 <saiganeshreddy.j@gmail.com>
2026-03-27 19:03:05 +0530

    add apiconfig

---
+++ src/config/ApiConfig.java
@@ -0,0 +1,6 @@
+package config;
+
+public class ApiConfig {
+    public static final String API_KEY = System.getenv("API_KEY");
+    public static final String BASE_URL = "https://api.football-data.org/v4";
+}
